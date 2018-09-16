0d3879f616ae92ea7ad50cfd84f38380b4ec819e
Henri Koskinen <hkoskinen@protonmail.com>
2018-09-16 14:53:11 +0300

    Add login form

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Form, Button } from 'semantic-ui-react';
 
 const LoginPage = () => (
   <div>
     <Container style={{ marginTop: '7em' }}>
       <Header as='h1'>Login</Header>
+
+      <Form>
+        <Form.Field>
+          <label htmlFor='username'>Username</label>
+          <input placeholder='username' id='username' />
+        </Form.Field>
+        <Form.Field>
+          <label htmlFor='password'>Password</label>
+          <input placeholder='password' id='password' type='password' />
+        </Form.Field>
+        <Button type='submit'>Log In</Button>
+      </Form>
+
     </Container>
   </div>
 );
