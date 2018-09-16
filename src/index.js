a65cacba4a9e8c7863847434447a0891d0223025
Henri Koskinen <hkoskinen@protonmail.com>
2018-09-16 13:33:04 +0300

    Add routing

--- src/index.js
+++ src/index.js
@@ -4,5 +4,26 @@ import 'semantic-ui-css/semantic.min.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const NotFound = () => (
+  <div>
+    <h1>404</h1>
+    <p>Page not found</p>
+  </div>
+);
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <h1>header</h1>
+      <Switch>
+        <Route path='/' component={App} exact />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
+  </Router>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
