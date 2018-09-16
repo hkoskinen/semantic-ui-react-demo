8cbd694adf21df70b04f7becd945e41a490b1788
Henri Koskinen <hkoskinen@protonmail.com>
2018-09-16 18:29:09 +0300

    Add user action

---
+++ src/actions/user.js
@@ -0,0 +1,41 @@
+
+export const USER_LOG_IN_REQUEST = 'USER_LOG_IN_REQUEST';
+export const USER_LOG_IN_FAILED = 'USER_LOG_IN_FAILED';
+export const USER_LOG_IN_SUCCESS = 'USER_LOG_IN_SUCCESS';
+
+const userLoginRequest = () => {
+  return {
+    type: USER_LOG_IN_REQUEST
+  };
+};
+
+const userLoginFailed = (error) => {
+  return {
+    type: USER_LOG_IN_FAILED,
+    payload: {
+      error
+    }
+  };
+};
+
+const userLoginSuccess = (data) => {
+  return {
+    type: USER_LOG_IN_SUCCESS,
+    payload: {
+      data
+    }
+  };
+};
+
+const userRequest = (user) => {
+  return (dispatch) => {
+    dispatch(userLoginRequest());
+
+    fetch(`${process.env.REACT_APP_NOTEBOOK_REST_API}/login`)
+      .then(res => {
+        dispatch(userLoginSuccess(res));
+      }).catch(err => {
+        dispatch(userLoginFailed(err));
+      });
+  };
+}
